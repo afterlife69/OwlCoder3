8fff850943e0e35f755fb119f9ce5b72f8fd266e
afterlife <104752943+afterlife69@users.noreply.github.com>
2024-01-06 23:35:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Techfest and the Queue/techfest-and-the-queue.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+	int sumOfPowers(int a, int b){
+	   int ans = 0;
+	   for(int i=a;i<=b;i++){
+	       int n = i;
+	       while(n%2==0){
+	           n/=2;
+	           ans++;
+	       }
+	       for(int j=3;j<=sqrt(n);j+=2){
+	           while(n%j==0){
+	               n/=j;
+	               ans++;
+	           }
+	       }
+	       if(n > 2)ans++;
+	   }
+	   return ans;
+	}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int a, b;
+		cin >>  a >> b;
+		Solution obj;
+		int ans = obj.sumOfPowers(a, b);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
