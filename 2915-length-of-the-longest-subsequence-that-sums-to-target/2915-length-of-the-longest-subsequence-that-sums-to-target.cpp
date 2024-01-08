807f7312764a067ea0722d167203931ea7d9eaf2
afterlife <104752943+afterlife69@users.noreply.github.com>
2024-01-08 21:13:09 +0530

    Time: 352 ms (70.86%), Space: 32 MB (69.49%) - LeetHub

---
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int dfs(int i,int tar,vector<int>&v){
+        if(i == v.size())return ((tar == 0)?0:-1e9);
+        if(tar == 0)return 0;
+        if(tar < 0)return -1e9;
+        if(dp[i][tar]!=-1)return dp[i][tar];
+        int ans = -1e9;
+        if((tar - v[i]) >= 0)ans = max(ans,dfs(i+1,tar-v[i],v))+1;
+        ans = max(ans,dfs(i+1,tar,v));
+        return dp[i][tar] = ans;
+    }
+    int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
+        int res = dfs(0,target,nums);
+        return res < 0 ? -1 : res;
+    }
+};
