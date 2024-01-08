ffdb37aaefcbd354e51ea868eefe28e4fa886e14
afterlife <104752943+afterlife69@users.noreply.github.com>
2024-01-08 23:02:43 +0530

    Time: 342 ms (73.88%), Space: 32 MB (69.49%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int dfs(int i,int tar,vector<int>&v){
-        if(i == v.size())return ((tar == 0)?0:-1e9);
-        if(tar == 0)return dp[i][tar] = 0;
-        if(tar < 0)return dp[i][tar] = -1e9;
-        if(dp[i][tar]!=-1)return dp[i][tar];
-        int ans = -1e9;
-        if((tar - v[i]) >= 0)ans = max(ans,dfs(i+1,tar-v[i],v))+1;
-        ans = max(ans,dfs(i+1,tar,v));
-        return dp[i][tar] = ans;
+    int ma=0;
+int dp[1001][1001];
+ int dfs(vector<int>&nums,int tar,int ind)
+{
+    if(ind>=nums.size() or tar==0)
+    {
+        if(tar==0)return 0;
+        return -1e9;
     }
+    if(dp[ind][tar]!=-1)return dp[ind][tar];
+    int x=-1e9,y=-1e9,res=0;
+    if(tar-nums[ind]>=0)x=dfs(nums,tar-nums[ind],ind+1)+1;
+    y=dfs(nums,tar,ind+1);
+    res=max(x,y);
+    return dp[ind][tar]=res;
+}
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+
         memset(dp,-1,sizeof(dp));
-        int res = dfs(0,target,nums);
-        return res < 0 ? -1 : res;
+       int res= dfs(nums,target,0);
+       return res>0?res:-1;
     }
 };
