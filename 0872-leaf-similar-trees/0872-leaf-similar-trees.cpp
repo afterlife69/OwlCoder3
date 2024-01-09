0a008e24219829b2a95e96a933a00cdd9339880b
afterlife <104752943+afterlife69@users.noreply.github.com>
2024-01-09 17:42:34 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (30.37%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* rt,vector<int>&v){
+        if(!rt)return;
+        if(!rt->left && !rt->right)v.push_back(rt->val);
+        dfs(rt->left,v);
+        dfs(rt->right,v);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int>v1,v2;
+        dfs(root1,v1);
+        dfs(root2,v2);
+        return v1 == v2;
+    }
+};
