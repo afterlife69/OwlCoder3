9618ccf796dd28d86701dfbe309c87c219765d52
afterlife <104752943+afterlife69@users.noreply.github.com>
2024-01-22 18:38:17 +0530

    Time: 2 ms (58.54%), Space: 7.3 MB (6.97%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -5,8 +5,11 @@ public:
         dp[0] = 1;
         dp[1] = 1;
         for(int i=2;i<=n;i++){
-            for(int j=0;j<i;j++){
-                dp[i]+= dp[j]*dp[i-j-1];
+            for(int j=0;j<(i%2==0 ? i/2 : i/2 + 1);j++){
+                if(j == (i - j - 1)){
+                    dp[i]+= dp[j] * dp[j];
+                }
+                else dp[i]+= 2 * dp[j]*dp[i-j-1];
             }
         }
         return dp[n];
