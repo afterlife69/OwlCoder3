fbc973da172dbdde0f581f71df2fdc65a0732cc4
afterlife <104752943+afterlife69@users.noreply.github.com>
2024-01-25 23:54:49 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (14.95%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[1001];
+    int dfs(int cur,vector<int>&v){
+        if(cur == 0)return 1;
+        if(cur < 0)return 0;
+        if(dp[cur] != -1)return dp[cur];
+        int ans = 0;
+        for(auto it : v){
+            if(cur - it < 0)break;
+            ans += dfs(cur-it,v);
+        }
+        return dp[cur] = ans;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
+        sort(nums.begin(),nums.end());
+        return dfs(target,nums);
+    }
+};
