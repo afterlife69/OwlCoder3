b6093acb8d77d18b85496f9e80a9eed7adc53df2
afterlife <104752943+afterlife69@users.noreply.github.com>
2024-01-31 10:56:49 +0530

    Time: 2693 ms (10.27%), Space: 11.7 MB (50.70%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
+    int mod = 1e9+7;
     int dp[1001][1001];
-    const int mod = 1e9 + 7;
-    int dfs(int n,int k){
-        if(n == 0)return k==0;
-        if(k==0)return 1;
-        if(k < 0)return 0;
-        if(dp[n][k] != -1)return dp[n][k];
-        int x = dfs(n-1,k);
-        int y = dfs(n,k-1);
-        int z = dfs(n-1,k-n);
-        return dp[n][k] = (((x + y) %mod + mod) - z)%mod;
+    int Fun(int n , int k){
+        //base case 
+        if(k <= 0)return k==0;
+        if(dp[n][k]!=-1)return dp[n][k]%mod;
+        int ans = 0;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(k - i >= 0)ans = (ans % mod+ Fun(n-1,k-i)%mod)%mod;
+        }
+        return dp[n][k]= ans%mod;
     }
     int kInversePairs(int n, int k) {
         memset(dp,-1,sizeof(dp));
-        return dfs(n,k);
+        return Fun(n,k);
+        
     }
 };
