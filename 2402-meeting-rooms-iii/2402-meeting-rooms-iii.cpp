b8ca1289344c73a7fbc5009ade375a333c5431c3
afterlife <104752943+afterlife69@users.noreply.github.com>
2024-02-19 00:35:30 +0530

    Time: 343 ms (73.51%), Space: 113.8 MB (26.29%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,21 +1,37 @@
-#define pii pair<int,int>
+#define pii pair<long long,long long>
 class Solution {
 public:
-    int mostBooked(int n, vector<vector<int>>& meets) {
-        int cnt[101] = {};
-        sort(begin(meets), end(meets));
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-        for (int i = 0; i < n; ++i)
-            pq.push({meets[0][0], i});
-        for (auto &m : meets) {
-            while (pq.top().first < m[0]) {
-                pq.push({m[0], pq.top().second});
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        priority_queue<pii,vector<pii>,greater<pii>>pq;
+        sort(meetings.begin(),meetings.end());
+        vector<int>ans(n);
+        int mx = 0, m = meetings.size();
+        priority_queue<int,vector<int>,greater<int>>rooms;
+        for(int i=0;i<n;i++){
+            rooms.push(i);
+        }
+        for(auto it : meetings){
+            while(!pq.empty() && pq.top().first <= it[0]){
+                rooms.push(pq.top().second);
                 pq.pop();
             }
-            auto [start, room] = pq.top(); pq.pop();
-            pq.push({start + m[1] - m[0], room});
-            ++cnt[room];
+            if(rooms.empty()){
+                long long cur = pq.top().first,rm = pq.top().second;
+                pq.pop();
+                ans[rm]++;
+                pq.push({it[1] - it[0] + cur,rm});
+                mx = max(mx,ans[rm]);
+                continue;
+            }
+            int cur = rooms.top();
+            rooms.pop();
+            ans[cur]++;
+            mx = max(mx,ans[cur]);
+            pq.push({it[1],cur});
         }
-        return max_element(begin(cnt), end(cnt)) - begin(cnt);
+        for(int i=0;i<n;i++){
+            if(ans[i] == mx)return i;
+        }
+        return 0;
     }
 };
