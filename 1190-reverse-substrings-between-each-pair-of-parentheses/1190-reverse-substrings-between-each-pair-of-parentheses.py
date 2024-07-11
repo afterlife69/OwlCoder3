ee84cb824fa7d4f53b60f125646b42d6eb652717
Harshith Akella <104752943+afterlife69@users.noreply.github.com>
2024-07-11 16:17:28 +0530

    Time: 38 ms (41.87%), Space: 16.5 MB (81.94%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,22 +1,12 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        st = []
-        v = [0] * len(s)
-        for i in range(len(s)):
-            if s[i] == '(':
-                st.append(i)
-            elif s[i] == ')':
-                v[st[-1]] = i
-                v[i] = st[-1]
-                st.pop()
-        i = 0
-        add = 1
-        ans = ''
-        while i < len(s):
-            if s[i] == '(' or s[i] == ')':
-                i = v[i]
-                add = -add
+        st = ['']
+        for i in s:
+            if i == '(':
+                st.append('')
+            elif i == ')':
+                cur = st.pop()[::-1]
+                st[-1] += cur
             else:
-                ans += s[i]
-            i += add
-        return ans
+                st[-1] += i
+        return st[-1]
