83bc160837580132870c80073e8c37e5d0668291
Harshith Akella <104752943+afterlife69@users.noreply.github.com>
2024-10-29 15:55:53 +0530

    Time: 3570 ms (59.81%), Space: 99.7 MB (11.00%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.py
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.py
@@ -1,13 +1,16 @@
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
         
-        dp = [[-1] * (27) for i in range(t+1)]
+        dp = [[-1] * (3) for i in range(t+1)]
         def dfs(cur, rem):
-            if dp[rem][cur] != -1:
-                return dp[rem][cur]
+            if cur <= 2:
+                if dp[rem][cur] != -1:
+                    return dp[rem][cur]
             if (cur + rem) > 26:
-                dp[rem][cur] = dfs(1, rem - abs(cur - 27)) + dfs(2, rem - abs(cur - 27))
-                return dp[rem][cur]
+                a = dfs(1, rem - abs(cur - 27)) + dfs(2, rem - abs(cur - 27))
+                if cur <= 2:
+                    dp[rem][cur] = a
+                return a
             else:
                 return 1
                 
