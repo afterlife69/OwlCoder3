8a04df5bc563354323133f5f61d0c911fc82369e
Harshith Akella <104752943+afterlife69@users.noreply.github.com>
2024-11-27 13:02:59 +0530

    Time: 479 ms (66.92%), Space: 17.7 MB (8.31%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,26 +1,21 @@
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         
-        dp = [0] * n
-        adj = [[] for i in range(n)]
-        k = 1
-        for i in range(n-1):
-            dp[n - i - 2] = k
-            k += 1
-            adj[i+1].append(i)
         ans = []
-        for u,v in queries:
-            adj[v].append(u)
-            dp[u] = min(dp[u], dp[v] + 1)
-            q = deque([(u, dp[u])])
-            vis = [0] * n
+        jump = [[i+1] for i in range(n)]
+        dist = [i for i in range(n)] 
+        for u, v in queries:
+            jump[u].append(v)
+            dist[v] = min(dist[v], dist[u] + 1)
+            q = deque([(0, 0)])
+            vis = set([0])
             while q:
                 node, lvl = q.popleft()
-                dp[node] = min(dp[node],lvl)
-                vis[node] = 1
-                for it in adj[node]:
-                    if not vis[it]:
-                        q.append((it,lvl+1))
-            ans.append(dp[0])
+                if node == n - 1:
+                    ans.append(lvl)
+                    break
+                for nei in jump[node]:
+                    if nei not in vis:
+                        q.append((nei, lvl + 1))
+                        vis.add(nei)
         return ans
-                
